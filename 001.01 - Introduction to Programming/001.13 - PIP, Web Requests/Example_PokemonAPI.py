1e7cdbfdc1277aed0a50b563e55091d9589c16b6
Graysen Rowland <67707202+graysenrowland@users.noreply.github.com>
2022-02-02 20:00:03 -0800

    sync

--- 001.01 - Introduction to Programming/001.13 - PIP, Web Requests/Example_PokemonAPI.py
+++ 001.01 - Introduction to Programming/001.13 - PIP, Web Requests/Example_PokemonAPI.py
@@ -1,8 +1,12 @@
 import json
 import requests
 
+
+
 method = "GET"
-url = "https://pokeapi.co/api/v2/gender/1"
+url = "https://pokeapi.co/api/v2/gender/1" # Female
+#url = "https://pokeapi.co/api/v2/gender/2" # Male 
+#url = "https://pokeapi.co/api/v2/gender/3" # Genderless
 headers = {}
 payload = ""
 response = requests.request(method, url, headers=headers, data=payload)
